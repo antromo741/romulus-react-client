47c074f270eb97b95460e9a1e55fe06713d10442
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-04-13 17:58:10 -0400

    added the index container, now will add the list.js

---
+++ src/containers/IndexContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import List from '../components/List'
+export default class IndexContainer extends Component {
+
+
+    state = {
+        groups: [],
+        loading: true,
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                groups: [
+                    { name: 'Anthony'},
+                    { name: 'Romo' }
+                ],
+                loading: false,
+            })
+        },1000)
+
+    }
+
+    render() {
+        return (
+            <section className="max-w-6xl mx-auto mt-16">
+                {this.state.loading ? (
+                    'loading spinner'
+                ) : (
+                    <List groups={this.state.groups} />
+                )}
+            </section>
+        )
+    }
+}
